16ffb94bc724a4e2508b0f4ae1834155623cc2e9
Jantar <davidbednarek@email.cz>
2023-03-24 16:45:38 +0100

    initial commit

---
+++ addComment.php
@@ -0,0 +1,48 @@
+<?php
+    include_once "commentSystem.php";
+
+
+    // Saving $_POST vars to local variables
+    $name = $_POST['comName'];
+    $replyToID = $_POST['replyToID'];
+    $text = $_POST['comText'];
+    $returnURL = $_POST['returnURL'];
+    $fileName = $_POST['fileName'];
+
+
+    // creates config file
+    $configPath = './comments/config.json';
+    if (!file_exists($configPath)) {
+        $json = json_encode(array(
+            'id' => 0
+        ));
+        file_put_contents($configPath, $json);
+    }
+    $config = json_decode(file_get_contents($configPath));
+
+
+    // Adds 1 to ID in config
+    $config->id = $config->id + 1;
+    file_put_contents($configPath, json_encode($config));
+
+
+    // Constructs Comment object
+    $comment = new Comment($config->id, date("d.m.Y h:i:s"), $replyToID == "" ? null : $replyToID, $name, $text);
+    $json = $comment->makeJSON();
+
+    mkdir('./comments'); // throws error if dir exists
+
+    $filePath = './comments/'.str_replace("/","-",$fileName).'-comments.json';
+
+
+    // appends new comment to a json file
+    $content = file_get_contents($filePath);
+    $temp = (array)json_decode($content);
+    array_push($temp, json_decode($json));
+    $jsonData = array(stripslashes(json_encode($temp, JSON_PRETTY_PRINT|JSON_UNESCAPED_UNICODE)));
+    file_put_contents($filePath, $jsonData);
+
+    
+    // return back (also refreshes the page so the new comment shows up)
+    header('Location: ' . $returnURL);
+?>
---
+++ commentSystem.php
@@ -0,0 +1,110 @@
+<?php
+
+class CommentSystem {
+    static function init() {
+        // CSS
+        print "
+        <head>
+            <link rel='stylesheet' href='commentSystem.css'>
+        </head>
+        ";
+
+
+        // Figure out URL of this location
+        $fileName = "$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+        $url = (empty($_SERVER['HTTPS']) ? 'http' : 'https') . "://$fileName";
+
+        $r = isset($_POST['reply']) ? $_POST['reply'] : "";
+
+        // Prints comment input
+        print "
+            <h1>Comments</h1>
+            <div id='comment-input-container'>
+                <label for='comName'>Name:</label>
+                <form method='POST' id='comment-form' action='addComment.php' accept-charset='UTF-8'>
+                    <input name='comName' placeholder='Name' value='Anonymous'></input>
+                    <input name='reply' placeholder='Reply to ID' value='$r'></input>
+                    <br>
+                    <textarea rows=7 name='comText' placeholder='Write your comment here...'></textarea>
+                    <br>
+                    <input name='returnURL' type='hidden' value =" . $url . "></input>
+                    <input name='fileName' type='hidden' value =" . $fileName . "></input>
+                    <input type='submit' value='send'></input>
+                </form>
+            </div>
+        ";
+
+        $filePath = './comments/'.str_replace("/","-",$fileName).'-comments.json';
+
+
+        // If there is comment JSON, print out all the comments
+        if (file_exists($filePath)) {
+            $json = json_decode(file_get_contents($filePath));
+            foreach ($json as $i) {
+                $wroteOrReplied = $i->replyToID ? "replied to #".$i->replyToID.":" : "wrote:";
+                if ($i->replyToID) {
+                    $originalComment = Comment::getItemByID($i->replyToID);
+                    print "
+                    <div id='comment-input-container'>
+                        <span id=comment-name>$originalComment->name </span><span id=comment-id>($originalComment->date) (#$originalComment->id)</span><span id=comment-classic> originally said:</span>
+                        <p id=comment-text>$originalComment->comment</p>
+                    </div>
+                ";
+                }
+                print "
+                    <div id='comment-input-container'>
+                        <span id=comment-name>$i->name </span><span id=comment-id>($i->date) (#$i->id)</span><span id=comment-classic> $wroteOrReplied</span>
+                        <p id=comment-text>$i->comment</p>
+                        <form method='POST' id='comment-form' actionm='./'>
+                            <input type='hidden' name='reply' value='$i->id'></input>
+                            <input id='comment-reply-button' type='submit' value='Reply'></input>
+                        </form>
+                        <hr>
+                    </div>
+                ";
+            }
+        }
+    }
+}
+
+class Comment {
+    private int $id;
+    private string $date;
+    private ?int $replyToID;
+    private string $name;
+    private string $comment;
+    
+    function __construct(int $id, string $date, ?int $replyToID, string $name, string $comment) {
+        $this->id = $id;
+        $this->date = $date;
+        $this->replyToID = $replyToID;
+        $this->name = $name;
+        $this->comment = $comment;
+    }
+
+    function makeJSON() {
+        $arr = array(
+            'id' => $this->id,
+            'date' => $this->date,
+            'replyToID' => $this->replyToID,
+            'name' => $this->name,
+            'comment' => $this->comment
+        );
+        $json = json_encode($arr, JSON_UNESCAPED_UNICODE);
+        $json = stripslashes($json);
+        return $json;
+    }
+
+    static function getItemByID(int $id) {
+        $fileName = "$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+        $filePath = './comments/'.str_replace("/","-",$fileName).'-comments.json';
+        $json = json_decode(file_get_contents($filePath));
+        foreach ($json as $i) {
+            if ($i->id == $id) {
+                return $i;
+            }
+        }
+        return null;
+    }
+}
+?>
